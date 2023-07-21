c757f5a9022bfb51f953ae491ef876789708d857
Edwardrem <rufaromutepfa@gmail.com>
2023-07-21 03:53:14 -0700

    a function that produces output

---
+++ task_0.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: character string
+ *
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+	char ch;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					ch = va_arg(args, int);
+					putchar(ch);
+					count++;
+					break;
+				case 's':
+					count += puts(va_arg(args, char *));
+					break;
+				case '%':
+					putchar('%');
+					count++;
+					break;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+
+	va_end(args);
+
+	return (count);
+}
