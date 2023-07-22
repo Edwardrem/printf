57e40038e1a2437d8ed24e2cf51a8fd7a0955ae3
Edwardrem <rufaromutepfa@gmail.com>
2023-07-22 13:21:30 -0700

    a function that produces output according to a format.

--- task_0.c
+++ task_0.c
@@ -10,10 +10,9 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int count = 0;
-	char ch;
+	char c;
 
 	va_start(args, format);
-
 	while (*format)
 	{
 		if (*format == '%')
@@ -22,28 +21,28 @@ int _printf(const char *format, ...)
 			switch (*format)
 			{
 				case 'c':
-					ch = va_arg(args, int);
-					putchar(ch);
+					c = (char)va_arg(args, int);
+					write(1, &c, 1);
 					count++;
 					break;
 				case 's':
-					count += puts(va_arg(args, char *));
+					count += write(1, va_arg(args, char *), strlen(va_arg(args, char *)));
 					break;
 				case '%':
-					putchar('%');
+					write(1, "%", 1);
 					count++;
+					break;
+				default:
 					break;
 			}
 		}
 		else
 		{
-			putchar(*format);
+			write(1, format, 1);
 			count++;
 		}
 		format++;
 	}
-
 	va_end(args);
-
 	return (count);
-}
+`}
