8525438b9eb2614eaff9dc1103ebe96a64b02f76
ABgitme <abebekezhabesha@gmail.com>
2023-07-25 09:53:11 -0700

    updated get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -10,21 +10,30 @@
  */
 int get_precision(const char *format, int *i, va_list list)
 {
+	int curr_i = *i + 1;
 	int precision = -1;
 
-	if (format[*i] == '.')
-	{
-		*i++;
-		if (is_digit(format[*i]))
-		{
+	if (format[curr_i] != '.')
+		return (precision);
 			precision = 0;
-			while (is_digit(format[*i]))
-			{
-				precision = precision * 10 + (format[*i] - '0');
-				*i++;
-			}
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
+	{
+		if (is_digit(format[curr_i]))
+		{
+			precision *= 10;
+			precision += format[curr_i] - '0';
 		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
 	}
+
+	*i = curr_i - 1;
 
 	return (precision);
 }
